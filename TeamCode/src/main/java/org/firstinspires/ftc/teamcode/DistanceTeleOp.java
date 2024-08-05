ef96d41e02d749ae38d03bc96ed0c2116e105a21
ryan C <147014849+luenix1@users.noreply.github.com>
2024-08-04 19:35:58 -0700

    TeleOp mode that drives & uses distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceTeleOp.java
@@ -2,17 +2,33 @@ package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.INCH;
 
+import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp
 public class DistanceTeleOp
 extends LinearOpMode {
+    IntegratingGyroscope gyro;
+    NavxMicroNavigationSensor navxMicro;
+    ElapsedTime timer = new ElapsedTime();
     @Override
     public void runOpMode() throws InterruptedException {
+        navxMicro = hardwareMap.get(NavxMicroNavigationSensor.class, "gyro");
+        gyro = (IntegratingGyroscope)navxMicro;
         DcMotor backRight=hardwareMap.get(DcMotor.class,"backRight");
         DcMotor backLeft=hardwareMap.get(DcMotor.class,"backLeft");
         DcMotor frontRight=hardwareMap.get(DcMotor.class,"frontRight");
@@ -24,14 +40,28 @@ extends LinearOpMode {
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         DistanceSensor sensor=hardwareMap.get(DistanceSensor.class,"distance");
+        ColorSensor ribbit = hardwareMap.get(ColorSensor.class, "colorSensor");
+
+        telemetry.log().add("Gyro Calibrating. Do Not Move!");
+
+        // Wait until the gyro calibration is complete
+        timer.reset();
+        while (navxMicro.isCalibrating())  {
+            telemetry.addData("calibrating", "%s", Math.round(timer.seconds())%2==0 ? "|.." : "..|");
+            telemetry.update();
+            Thread.sleep(50);
+        }
+        telemetry.log().clear(); telemetry.log().add("Gyro Calibrated. Press Start.");
+        telemetry.clear(); telemetry.update();
 
 
         waitForStart();
+        telemetry.log().clear();
 
 
         if (isStopRequested()) return;
         double distance = 100;
-        while (opModeIsActive() && distance>10) {
+        while (opModeIsActive()); {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
@@ -50,13 +80,38 @@ extends LinearOpMode {
             frontRight.setPower(frontRightPower/2);
             backRight.setPower(backRightPower/2);
 
+            double color= ribbit.argb();
             distance=sensor.getDistance(INCH);
             telemetry.addData("distance", distance);
+            telemetry.addData("color",color);
+
+            AngularVelocity rates = gyro.getAngularVelocity(AngleUnit.DEGREES);
+            Orientation angles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+            telemetry.addLine()
+                    .addData("dx", formatRate(rates.xRotationRate))
+                    .addData("dy", formatRate(rates.yRotationRate))
+                    .addData("dz", "%s deg/s", formatRate(rates.zRotationRate));
+
+            telemetry.addLine()
+                    .addData("heading", formatAngle(angles.angleUnit, angles.firstAngle))
+                    .addData("roll", formatAngle(angles.angleUnit, angles.secondAngle))
+                    .addData("pitch", "%s deg", formatAngle(angles.angleUnit, angles.thirdAngle));
             telemetry.update();
+
         }
-        frontLeft.setPower(0);
-        backLeft.setPower(0);
-        frontRight.setPower(0);
-        backRight.setPower(0);
+
+    }
+
+    String formatRate(float rate) {
+        return String.format("%.3f", rate);
+    }
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format("%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
 }
