a40a47a86c2be92ff0caec78614591abdbb65a0b
ryan C <147014849+luenix1@users.noreply.github.com>
2024-09-22 19:52:15 -0700

    yippee the color sensor now works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferHardware.java
@@ -58,7 +58,7 @@ public class StraferHardware extends HardwareMapper {
     public CameraName Webcam1;
 
     @HardwareName("ribbit")
-    public ColorSensor ribbit;
+    public ColorSensor colorSensor;
   //  public Servo hand;
 
 }
