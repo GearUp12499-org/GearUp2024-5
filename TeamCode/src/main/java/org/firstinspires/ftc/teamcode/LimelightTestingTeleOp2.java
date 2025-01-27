c7a2fb17fcd8d9917eeb953506a1fb7e72f82599
shuban-789 <cybergeek11929@gmail.com>
2025-01-26 16:55:38 -0800

    limelight testing teleop 2 with active angle retrieval (hello world starter for openCV angle tracking)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTestingTeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTestingTeleOp2.java
@@ -11,70 +11,55 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Arrays;
 
 @TeleOp
 public class LimelightTestingTeleOp2 extends LinearOpMode {
     private Hardware hardware;
-
     private Limelight3A limelight;
 
     @Override
     public void runOpMode() {
         hardware = new Hardware(hardwareMap);
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        double[] config = {1.0, 1.0, 0.0}; // Red, Yellow, Blue
+        double[] config = {1.0, 1.0, 0.0};
         limelight.updatePythonInputs(config);
         limelight.setPollRateHz(100);
         telemetry.setMsTransmissionInterval(11);
-        limelight.pipelineSwitch(3); // Configure to the correct pipeline number
+        limelight.pipelineSwitch(3);
         limelight.start();
-        hardware.clawTwist.setPosition(Hardware.CLAW_TWIST_INIT);
 
         waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            // TO-DO: Start a timer
-            if (gamepad1.a) {
-                hardware.limelightlight.setPosition(1);
-                sleep(500);
-                boolean foundAngle = false;
-                double angle;
-                while(!foundAngle) { //TO-DO: set a timer
-                    LLResult result = limelight.getLatestResult();
+            LLResult result = limelight.getLatestResult();
 
-                    if (result != null /* && result.isValid() */) {
-                        // Pose3D botpose = result.getBotpose();
-                        telemetry.addData("tx", result.getTx());
-                        telemetry.addData("ty", result.getTy());
-                        telemetry.addData("Avg Area: ", result.getTa());
-                        telemetry.addData("Pipeline Number: ", limelight.getStatus().getPipelineIndex());
-
-                        double[] pythonOutputs = result.getPythonOutput();
-                        if (pythonOutputs != null && pythonOutputs.length > 6) {
-                            foundAngle = true;
-                            angle = pythonOutputs[0];
-                            telemetry.addData("angle", angle);
-                        }
-                    }
-                }
+            if (result == null) {
+                telemetry.addData("Limelight", "result is null!");
             }
 
+            if (result != null /* && result.isValid() */) {
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("Avg Area: ", result.getTa());
+                telemetry.addData("Pipeline Number: ", limelight.getStatus().getPipelineIndex());
+
+                double[] pythonOutputs = result.getPythonOutput();
+                if (pythonOutputs != null && pythonOutputs.length > 6) {
+                    double angle = pythonOutputs[0];
+                    telemetry.addData("angle", angle);
+                }
+            } else {
+                telemetry.addData("Limelight", "No data available");
+            }
 
             telemetry.update();
-
-            //hardware.clawTwist.setPosition(ServoAngle(getAngle(getLongPair())));
         }
     }
 }
