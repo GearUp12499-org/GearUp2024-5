498631890098cd813c7c20d3c1d42dbd94acd926
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2025-02-09 15:27:56 -0800

    send it (worse)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAuto.java
@@ -120,7 +120,7 @@ public class RightAuto extends LinearOpMode {
         speed2Power = new Speed2Power(0.20); // Set a speed2Power corresponding to a speed of 0.20 seconds
         ramps = new Ramps(
                 Ramps.linear(5.0),
-                Ramps.linear(1 / 12.0),
+                Ramps.linear(1 / 4.0),
 //                Easing.power(3.0, 12.0),
                 Ramps.LimitMode.SCALE
         );
@@ -206,7 +206,7 @@ public class RightAuto extends LinearOpMode {
                 .then(vLiftProxy.moveTo(0, 5, .25)));
     }
 
-    private ITask scoreSpecimen() {
+    private ITask  scoreSpecimen() {
         return groupOf(it -> it.add(run(() -> hardware.wrist.setPosition(1)))
                 .then(await(300))
                 .then(moveRel(new Pose(-4.0, 0, 0)))
